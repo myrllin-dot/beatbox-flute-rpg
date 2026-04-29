912cbe40a387510c67adccecee6cc5030b47cc98
myrllin-dot <myrllincheng@gmail.com>
2026-04-29 14:10:13 +0800

    Set TRPC API URL from environment variable

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../../../server/routers";
-
+const url = import.meta.env.VITE_API_URL || "/api/trpc";
 export const trpc = createTRPCReact<AppRouter>();
